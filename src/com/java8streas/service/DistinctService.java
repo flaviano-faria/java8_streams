19b933f1b25d6a93f42a6874f4e19ce36af0ca46
flaviano-faria <flsilv89@yahoo.com.br>
2024-11-29 23:16:13 -0300

    Distincts and set

--- src/com/java8streas/service/DistinctService.java
+++ src/com/java8streas/service/DistinctService.java
@@ -1,7 +1,7 @@
 package com.java8streas.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class DistinctService {
@@ -10,6 +10,12 @@ public class DistinctService {
 		List<Integer> integerList = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
 		List<Integer> filteredList = integerList.stream().distinct().collect(Collectors.toList());
 		return filteredList;
+	}
+	
+	public Set getDistinctWithSet() {
+		List<Integer> integerList = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+		Set<Integer> filteredSet = integerList.stream().collect(Collectors.toSet());
+		return filteredSet;
 	}
 
 }
