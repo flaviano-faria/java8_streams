b04cf5c4621f8625721ba9b479beac8062af1f07
flaviano-faria <flsilv89@yahoo.com.br>
2024-12-01 17:22:25 -0300

    find first service

---
+++ src/com/java8streas/service/FindFirstService.java
@@ -0,0 +1,21 @@
+package com.java8streas.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FindFirstService {
+	
+	public Integer findFirstOcurrency() {
+		Optional<Integer> filteredItem = 
+				Stream.of(1,2,3,4,5,5,5,6).filter(n -> n==5).findFirst();
+		
+		if(filteredItem.isPresent()) {
+			return filteredItem.get();
+		}
+		
+		return 0;
+	}
+
+}
