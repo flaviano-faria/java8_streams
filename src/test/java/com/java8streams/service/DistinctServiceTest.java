3435eae609ebb4e2be5d80ed547f1cfd94ce2136
flaviano-faria <flsilv89@yahoo.com.br>
2024-12-24 14:42:03 -0300

    distinctServiceTest addes

---
+++ src/test/java/com/java8streams/service/DistinctServiceTest.java
@@ -0,0 +1,29 @@
+package com.java8streams.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class DistinctServiceTest {
+	
+	@Test
+	public void getDistinctIntegersTest() {
+		List<Integer> expectedList = new ArrayList<Integer>();
+		expectedList.add(1);
+		expectedList.add(2);
+		expectedList.add(3);
+		expectedList.add(4);
+		expectedList.add(5);
+		expectedList.add(6);
+		expectedList.add(7);
+		expectedList.add(8);
+		expectedList.add(9);
+		
+		DistinctService distinctService = new DistinctService();
+		assertEquals(expectedList, distinctService.getDistinctIntegers());
+	}
+
+}
