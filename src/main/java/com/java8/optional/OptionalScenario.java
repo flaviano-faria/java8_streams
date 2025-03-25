a0b0b1b11ec366ba268b3e88deade794131484c9
flaviano-faria <flsilv89@yahoo.com.br>
2025-03-24 21:38:05 -0300

    Improving optional

--- src/main/java/com/java8/optional/OptionalScenario.java
+++ src/main/java/com/java8/optional/OptionalScenario.java
@@ -1,5 +1,7 @@
 package com.java8.optional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class OptionalScenario {
@@ -11,6 +13,16 @@ public class OptionalScenario {
 		if(optionalStr.isPresent()){
 			System.out.println(optionalStr.get());
 		}
+		
+		String name = null;
+		String nameString  = Optional.ofNullable(name).orElse("");
+		System.out.println(nameString);
+		
+		List<Optional<String>> listString = new ArrayList<Optional<String>>();
+
+		
+		boolean result = listString.stream().anyMatch(Optional::isPresent);
+		System.out.println(result);
 	}
 
 }
